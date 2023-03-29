068871c79dd427c2b8f6a708169a9a9dfa30660e
msquaredds <alex.melesko@msquaredds.com>
2023-03-29 12:28:16 -0400

    Adding a private directory

--- app.py
+++ app.py
@@ -18,16 +18,34 @@ def main():
         help="This is just a test, so the correct password is 'password'.")
 
     if password == 'password':
+        # See if the password is generally correct
         st.write("You have the correct password!")
+
+        # Get the private directory if it doesn't already exist
         if not os.path.exists('./private'):
             git_pat = st.secrets['GIT_PAT']
             repo_name = "https://msquaredds:" + git_pat +\
                         "@github.com/msquaredds/PrivateGitForPublicApp.git"
             Repo.clone_from(repo_name, "./private")
-        st.write("Directory:")
-        st.write(os.listdir(os.path.abspath(os.getcwd())))
+        #st.write("Directory:")
+        #st.write(os.listdir(os.path.abspath(os.getcwd())))
+        # Import the private code
         from private import main
         st.markdown(main.private_text)
+
+        # Do the same with a private directory
+        if not os.path.exists('./private_dir'):
+            git_pat = st.secrets['GIT_PAT']
+            # This is a more specific directory within a repo
+            repo_name = "https://msquaredds:" + git_pat +\
+                        "@github.com/msquaredds/PrivateGitForPublicApp" \
+                        "/private_dir.git"
+            Repo.clone_from(repo_name, "./private_dir")
+        st.write("Directory:")
+        st.write(os.listdir(os.path.abspath(os.getcwd())))
+        # Import the private code
+        import private_dir
+        private_dir.test.say_hi()
 
 
 
